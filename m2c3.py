ebd99a66185bf405504b29f951befcc206cac218
koldoarretxea <koldo.arretxea@gmail.com>
2023-01-31 19:07:37 +0100

    Commit3

--- m2c3.py
+++ m2c3.py
@@ -1,1 +1,33 @@
-nombre = 'Koldo'
+# Exercise 1
+
+nombre = 'Koldo Arretxea'
+numero = 1977
+lista = ['rojo', 'verde', 'amarillo', 'azul']
+hombre = True
+
+# Exercise 2
+
+ex2 = nombre [:3]
+print(ex2)
+
+# Exercise 3
+
+first_element = lista[0]
+print(first_element)
+
+# Exercise 4
+
+nuevo_numero = numero + 10
+print(nuevo_numero)
+
+
+# Exercise 5
+
+last_element = lista[-1]
+print(last_element)
+
+# Exercise 6
+
+names = 'harry,alex,susie,jared,gail,conner'
+nueva_lista = names.split(",")
+print(nueva_lista)
