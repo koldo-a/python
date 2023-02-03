be80ab092134d5e9e3fc374867379f30b578c5bc
koldoarretxea <koldo.arretxea@gmail.com>
2023-02-03 10:23:57 +0100

    initial commit

---
+++ m2c4.py
@@ -0,0 +1,43 @@
+# Exercise 1
+
+lista = ['manzana', 'pera', 'fresa', 'piña']
+print(lista)
+
+tuple = ('manzana', 'pera', 'fresa', 'piña')
+print(tuple)
+
+num1 = float(3)
+print(num1)
+
+num1 = int(3.8675345)
+print(num1)
+
+
+# Exercise 2
+
+
+
+# Exercise 3
+
+
+
+# Exercise 4
+
+
+
+
+# Exercise 5
+
+
+
+# Exercise 6
+
+
+# Exercise 7
+
+
+# Exercise 8
+
+
+# Exercise 9
+
