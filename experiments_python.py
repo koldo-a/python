7cdc87cbef89b7bcbd20f894cfd771c4e45ef70e
koldoarretxea <koldo.arretxea@gmail.com>
2023-02-15 18:25:14 +0100

    hok

---
+++ experiments_python.py
@@ -0,0 +1,12 @@
+
+
+def mayorqcero(param):
+    
+        if param > 0:
+            print('es mayor')
+        else: 
+            print('NO es mayor')
+            fin = False
+
+num = int(input("introduce un número: \n"))
+mayorqcero(num)
